fb2ffa704068c10eebfcacf9205e871bbefbfd7e
Guangchu Shi <rollschild@protonmail.com>
2025-09-04 21:57:27 -0400

    initial Lua bindings

---
+++ assets/scripts/script.lua
@@ -0,0 +1,31 @@
+-- This is a lua global variable
+some_var = 7 * 6
+user_name = "Guangchu"
+print("Hello " .. user_name)
+
+if some_var == 42 then
+  print("value of var is " .. some_var)
+end
+
+-- Lua table
+config = {
+  title = "C++ 2D Game Engine",
+  fullscreen = false,
+  resolution = {
+    width = 1800,
+    height = 1200,
+  },
+}
+
+entities = {
+  tank = {
+    transform = {
+      position = { x = 300, y = 200 },
+      scale = {},
+    },
+    box_collider = {},
+    projectile_emitter = {
+      angle = rand(0, 360),
+    },
+  },
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
-#include "./game/game.h"
 #include <iostream>
+#include <sol/sol.hpp>
+#include <sol/table.hpp>
+#include <sol/types.hpp>
+
+#include "./game/game.h"
+// #include <sol/state.hpp>
+
+void test_lua() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+    lua.script_file("./assets/scripts/script.lua");
+    int some_var_inside_cpp = lua["some_var"];
+    std::cout << "Value from lua script: " << some_var_inside_cpp << std::endl;
+
+    bool is_fullscreen = lua["config"]["fullscreen"];
+    sol::table config = lua["config"];
+    int width = config["resolution"]["width"];
+    int height = config["resolution"]["height"];
+    std::cout << "We read the value fullscreen: " << is_fullscreen << std::endl;
+    std::cout << "window width: " << width << std::endl;
+    std::cout << "window height: " << height << std::endl;
+}
 
 int main(/* int argc, char *argv[] */) {
-    Game game;
-    game.initialize();
-    game.run();
-    game.destroy();
+    // Game game;
+    // game.initialize();
+    // game.run();
+    // game.destroy();
+
+    test_lua();
     return 0;
 }
